42222f6bfa89b70d6283a2239c16d2a524fb1b27
viniciusneri3A1 <65737980+viniciusneri3A1@users.noreply.github.com>
2020-09-12 19:28:22 -0300

    Update AssemblyInfo.cs

--- POO3A196/POO3A10/Properties/AssemblyInfo.cs
+++ POO3A196/POO3A10/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("POO3A10")]
+[assembly: AssemblyTitle("POO3A196")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("POO3A10")]
+[assembly: AssemblyProduct("POO3A196")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
